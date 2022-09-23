b85908c95290ad2941e2e689f8a68b42af5a5133
SpinninJayWE <1315025826@qq.com>
2022-09-23 09:06:42 +0800

    更新代码

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import './css/public.css'
 import './css/index.css'
-import './css/login.css'
 
 import 'jquery'
 import './js/public'
 import './js/nav'
+
+// treeShaking触发条件
+// 1. 通过解构的方式获取方法，可以触发treeShaking
+// 2. 调用的npm包 必须使用ESM规范
+// 3. 同一文件的treeSshaking有触发条件， 条件就是mode=production
+// 4. 一定主要使用解构来加载模块
+
+import { get } from 'lodash-es'
+
+
+console.log(get({a:1}, 'a'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,45 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
-
+const CopyPlugin = require("copy-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const htmlWebpackPlugins = [
     new HtmlWebpackPlugin({
         filename: 'index.html',
-        template: './src/index.html'
+        template: './src/index.html',
+        chunks:['index']
     }),
     new HtmlWebpackPlugin({
         filename: 'login.html',
-        template: './src/login.html'
+        template: './src/login.html',
+        chunks:['login']
     })
 ]
 module.exports = {
     mode: 'development',
     entry: {
-        bundle: './src/index.js'
+        index: './src/index.js',
+        login: './src/login.js'
     },
     output: {
         filename: 'js/[name].js',
-        path: path.resolve(__dirname, './dist')
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist')
+        },
+        compress: true,
+        port: 9000,
+        hot: true
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -41,11 +55,49 @@ module.exports = {
             }
         ]
     },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new UglifyJsPlugin({
+                sourceMap: true
+            }),
+            new CssMinimizerPlugin()
+        ],
+        splitChunks: {
+            chunks: 'all',
+            minSize: 300 * 1024,
+            name: 'common',
+            cacheGroups: {
+                jquery: {
+                    name: 'jquery',
+                    test: /jquery\.js/,
+                    chunks: 'all'
+                },
+                'lodash-es': {
+                    name: 'lodash-es',
+                    test: /lodash-es/,
+                    chunks: 'all'
+                }
+            }
+        }
+    },
     plugins: [
         ...htmlWebpackPlugins,
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/img'),
+                    to: path.resolve(__dirname, 'dist/img')
+                }
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+            chunkFilename: 'css/[name].chunk.css'
         })
     ]
 }
